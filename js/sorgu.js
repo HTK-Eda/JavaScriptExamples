76fae0152282b8f29010297229789d9b2d021c3c
Eda KAYGULU <98541307+HTK-Eda@users.noreply.github.com>
2024-06-22 13:57:58 +0300

    Add files via upload

---
+++ js/sorgu.js
@@ -0,0 +1,52 @@
+/*Örnek:Sadece pozitif sayıların karesini alan program
+var sayi, kare;
+sayi=Number(prompt("Sayıyı gir:"));
+if(sayi>0){
+    kare=sayi*sayi;
+    alert(sayi+" sayısının karesi:"+kare);
+}
+else{
+    alert("Sayı pozitif değil. Yeni sayı giriniz");
+}  */
+
+/*Örnek:Girilen iki sayının durumunu bulan program
+var num1,num2,buyuk;
+num1=Number(prompt("1.sayı:"));
+num2=Number(prompt("2.sayı:"));
+if(num1>num2){
+    alert("1.sayı olan "+num1+" büyük");
+}
+else if(num2>num1){
+    alert("2.sayı olan "+num2+" büyük");
+}
+else{
+    alert("İki sayı birbirine eşit");
+}   */
+
+/*Örnek:Not ortalaması 50 altında ise kaldı değilse geçti yazdıran program
+var not1,not2,ort;
+not1=Number(prompt("Vize notu:"));
+not2=Number(prompt("Final notu:"));
+ort=(not1+not2)/2;
+if(ort<50){
+    alert("Kaldı Ort:"+ort);
+}
+else{
+    alert("Geçti Ort:"+ort);
+}   */
+/*Örnek:Hava sıcaklığını derecelendiren uygulama
+var sicaklik;
+sicaklik=Number(prompt("Haca sıcaklığını giriniz:"));
+if(sicaklik<0){
+    alert("Dondurucu");
+}
+else if(sicaklik<15){
+    alert("Soğuk");
+}
+else if(sicaklik<25){
+    alert("Ilık");
+}
+else{
+    alert("Sıcak");
+}    */
+
